2be73de90c0b4fe2cd6dc40696a456e41ba210e9
krzymich24 <krzysztof.michalak1999@gmail.com>
2023-10-25 23:04:52 +0200

    replace button and fixing size of nav and main in css and js

--- TST/script.js
+++ TST/script.js
@@ -20,16 +20,16 @@ document.addEventListener("DOMContentLoaded", function() {
 
  // Function to start JavaScript when the div is clicked
  function startJavaScript() {
-    alert('JavaScript started!');
+    //alert('JavaScript started!');
     // Add your JavaScript code here
     
     if (main.style.width == '83%'){
        // Add a 1-second delay before changing the width
       setTimeout(function() {
-        main.style.width = '96%';
-        nav.style.width = '4%';
-        container.style.width = '4%';
-        container.style.minHeight = '50px';
+        main.style.width = `calc(100% - 50px)`;
+        nav.style.width = '50px';
+        container.style.width = '50px';
+        container.style.minHeight = '45px';
       }, 1000); // 1000 milliseconds (1 second)
     }else{
       main.style.width = '83%';
