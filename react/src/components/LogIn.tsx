0f444ee0ecb81c866b88f10e0e272018c133d1d4
krzymich24 <krzysztof.michalak1999@gmail.com>
2023-11-08 18:31:07 +0100

    login error message

--- react/src/components/LogIn.tsx
+++ react/src/components/LogIn.tsx
@@ -31,6 +31,10 @@ export function LogIn() {
         <div className="card w-full max-w-sm shrink-0 bg-base-100 shadow-2xl">
           <form className="card-body" onSubmit={(e) => e.preventDefault()}>
             <div className="form-control">
+              <div className="alert alert-warning">
+                <svg xmlns="http://www.w3.org/2000/svg" className="stroke-current shrink-0 h-6 w-6" fill="none" viewBox="0 0 24 24"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" /></svg>
+                <span className="label-text-login">Warning: Bad login input!</span>
+              </div>
               <label className="label">
                 <span className="label-text">Username</span>
               </label>
