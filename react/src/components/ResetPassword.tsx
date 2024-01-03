b3eb4d0474083166b1e053205b972af0c0bd59a5
krzymich24 <krzysztof.michalak1999@gmail.com>
2024-01-03 18:47:00 +0100

    Add set new password

--- react/src/components/ResetPassword.tsx
+++ react/src/components/ResetPassword.tsx
@@ -1,64 +1,52 @@
- import { useState } from 'react';
- import { useParams } from "react-router-dom";
+import { useState } from 'react';
+import { useParams } from "react-router-dom";
+import { API } from "../api";
 
 export function ResetPassword() {
   const { token } = useParams();
-  const [username, setUsername] = useState<string>();
+  const [password, setPassword] = useState<string>('');
+
+  const setNewPassword = async () => {
+    try {
+      console.log('Password:', password);
+       const { data } = await API.put(`/person/reset?password=${password}`, { password });
+       console.log(data);
+    } catch (e) {
+      console.error((e as Error).message);
+    }
+  };
 
   return (
-    <>
-      <div className="hero min-h-screen bg-base-200">
-        <div className="hero-content flex-col lg:flex-row-reverse">
-          <div className="card w-full max-w-sm flex-shrink-0 bg-base-100 shadow-2xl">
-            <form className="card-body">
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Email</span>
-                </label>
-                <input
-                  type="email"
-                  placeholder="email"
-                  className="input input-bordered"
-                  required
-                  onChange={(event) => {
-                    setUsername(event.target.value);
-                  }}
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Username</span>
-                </label>
-                <input
-                  type="email"
-                  placeholder="username"
-                  className="input input-bordered"
-                  required
-                  onChange={(event) => {
-                    setUsername(event.target.value);
-                  }}
-                />
-              </div>
-              <div className="form-control">
-                <label className="label">
-                  <span className="label-text">Password</span>
-                </label>
-                <input type="password" placeholder="password" className="input input-bordered" required />
-              </div>
-              <div className="form-control mt-6">
-                <button
-                  className="btn btn-primary"
-                  onClick={() => {
-                    console.log({ username });
-                  }}
-                >
-                  Register Now!
-                </button>
-              </div>
-            </form>
-          </div>
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="card w-full max-w-sm shrink-0 bg-base-100 shadow-2xl">
+          <form
+            className="card-body"
+            onSubmit={(e) => {
+              e.preventDefault();
+            }}
+          >
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">New password</span>
+              </label>
+              <input
+                type="password"
+                placeholder=""
+                className="input-bordered input"
+                required
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div className="form-control mt-6">
+              <button className="btn-primary btn" type="button" onClick={setNewPassword}>
+                Set new password
+              </button>
+            </div>
+          </form>
         </div>
       </div>
-    </>
+    </div>
   );
 }
