fa5279b7ba364deaf17631662f38c2e9fce49d70
krzymich24 <krzysztof.michalak1999@gmail.com>
2024-01-03 16:39:28 +0100

    Update United.tsx

--- react/src/components/walls/United.tsx
+++ react/src/components/walls/United.tsx
@@ -36,6 +36,304 @@ export function United() {
           </li>
         </ul>
       </div>
+
+      <div className="overflow-x-auto">
+        <table className="table" style={{ width: '100%' }}>
+          {/* head */}
+          <thead>
+          <tr>
+            <th style={{ textAlign: 'center', fontSize: '15px', fontWeight: 'bold' }}>ID</th>
+            <th style={{ textAlign: 'center', fontSize: '15px', fontWeight: 'bold' }}>Nazwa</th>
+            <th style={{ textAlign: 'center', fontSize: '15px', fontWeight: 'bold' }}>Autor</th>
+            <th style={{ textAlign: 'center', fontSize: '15px', fontWeight: 'bold' }}>Poziom trudności</th>
+            <th style={{ textAlign: 'center', fontSize: '15px', fontWeight: 'bold' }}>Rating</th>
+            <th></th>
+          </tr>
+          </thead>
+          <tbody>
+          {/* row 1 */}
+          <tr>
+            <td style={{ textAlign: 'center' }}>
+              <div className="indicator">
+                <span className="indicator-item badge badge-primary" style={{fontWeight: 'bold'}}>new</span>
+                <div className="grid w-8 h-8 bg-base-100 place-items-center" style={{fontSize: '30px'}}>1</div>
+              </div>
+            </td>
+            <td style={{ textAlign: 'center' }}>
+              <div className="flex items-center justify-center">
+                <div className="avatar">
+                  <div className="mask w-40 h-40 relative">
+                    <span className="indicator-item indicator-top indicator-start badge badge-primary" style={{ fontSize: '20px', fontWeight: 'bold' }}>Begginer turn</span>
+                    <img src="src/assets/img/CS.jpg" alt="Climbing Spot" />
+                  </div>
+                </div>
+              </div>
+            </td>
+            <td style={{ textAlign: 'center', fontSize: '30px' }}>Tosia</td>
+            <td style={{ textAlign: 'center', fontSize: '30px' }}>Biały</td>
+            <td style={{ textAlign: 'center' }}>
+              <div className="stats shadow">
+
+                <div className="stat">
+                  <div className="stat-figure text-primary">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
+                  </div>
+                  <div className="stat-title">Polubień</div>
+                  <div className="stat-value text-primary">25.6K</div>
+                </div>
+
+                <div className="stat">
+                  <div className="stat-figure text-secondary">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
+                  </div>
+                  <div className="stat-title">Ukończyło</div>
+                  <div className="stat-value text-secondary">29k</div>
+                </div>
+
+              </div>
+            </td>
+            <th>
+                <button className="btn btn-primary">Oceń i oznacz</button>
+            </th>
+          </tr>
+          {/* row 2 */}
+          <tr>
+            <td style={{ textAlign: 'center' }}>
+              <div className="indicator">
+                <span className="indicator-item badge badge-secondary" style={{fontWeight: 'bold'}}>modified</span>
+                <div className="grid w-8 h-8 bg-base-100 place-items-center" style={{fontSize: '30px'}}>2</div>
+              </div>
+            </td>
+            <td style={{ textAlign: 'center' }}>
+              <div className="flex items-center justify-center">
+                <div className="avatar">
+                  <div className="mask w-40 h-40 relative">
+                    <span className="indicator-item indicator-top indicator-start badge badge-primary" style={{ fontSize: '20px', fontWeight: 'bold' }}>Begginer turn</span>
+                    <img src="src/assets/img/CS.jpg" alt="Climbing Spot" />
+                  </div>
+                </div>
+              </div>
+            </td>
+            <td style={{ textAlign: 'center', fontSize: '30px' }}>Tosia</td>
+            <td style={{ textAlign: 'center', fontSize: '30px' }}>Biały</td>
+            <td style={{ textAlign: 'center' }}>
+              <div className="stats shadow">
+
+                <div className="stat">
+                  <div className="stat-figure text-primary">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
+                  </div>
+                  <div className="stat-title">Polubień</div>
+                  <div className="stat-value text-primary">25.6K</div>
+                </div>
+
+                <div className="stat">
+                  <div className="stat-figure text-secondary">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
+                  </div>
+                  <div className="stat-title">Ukończyło</div>
+                  <div className="stat-value text-secondary">29k</div>
+                </div>
+
+              </div>
+            </td>
+            <th>
+              <button className="btn btn-primary">Oceń i oznacz</button>
+            </th>
+          </tr>
+          {/* row 3 */}
+          <tr>
+            <td style={{ textAlign: 'center' }}>
+              <div className="indicator">
+                <div className="grid w-8 h-8 bg-base-100 place-items-center" style={{fontSize: '30px'}}>3</div>
+              </div>
+            </td>
+            <td style={{ textAlign: 'center' }}>
+              <div className="flex items-center justify-center">
+                <div className="avatar">
+                  <div className="mask w-40 h-40 relative">
+                    <span className="indicator-item indicator-top indicator-start badge badge-primary" style={{ fontSize: '20px', fontWeight: 'bold' }}>Begginer turn</span>
+                    <img src="src/assets/img/CS.jpg" alt="Climbing Spot" />
+                  </div>
+                </div>
+              </div>
+            </td>
+            <td style={{ textAlign: 'center', fontSize: '30px' }}>Tosia</td>
+            <td style={{ textAlign: 'center', fontSize: '30px' }}>Biały</td>
+            <td style={{ textAlign: 'center' }}>
+              <div className="stats shadow">
+
+                <div className="stat">
+                  <div className="stat-figure text-primary">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
+                  </div>
+                  <div className="stat-title">Polubień</div>
+                  <div className="stat-value text-primary">25.6K</div>
+                </div>
+
+                <div className="stat">
+                  <div className="stat-figure text-secondary">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
+                  </div>
+                  <div className="stat-title">Ukończyło</div>
+                  <div className="stat-value text-secondary">29k</div>
+                </div>
+
+              </div>
+            </td>
+            <th>
+              <button className="btn btn-primary">Oceń i oznacz</button>
+            </th>
+          </tr>
+          {/* row 4 */}
+          <tr>
+            <td style={{ textAlign: 'center' }}>
+              <div className="indicator">
+                <span className="indicator-item badge badge-primary" style={{fontWeight: 'bold'}}>new</span>
+                <div className="grid w-8 h-8 bg-base-100 place-items-center" style={{fontSize: '30px'}}>4</div>
+              </div>
+            </td>
+            <td style={{ textAlign: 'center' }}>
+              <div className="flex items-center justify-center">
+                <div className="avatar">
+                  <div className="mask w-40 h-40 relative">
+                    <span className="indicator-item indicator-top indicator-start badge badge-primary" style={{ fontSize: '20px', fontWeight: 'bold' }}>Begginer turn</span>
+                    <img src="src/assets/img/CS.jpg" alt="Climbing Spot" />
+                  </div>
+                </div>
+              </div>
+            </td>
+            <td style={{ textAlign: 'center', fontSize: '30px' }}>Tosia</td>
+            <td style={{ textAlign: 'center', fontSize: '30px' }}>Biały</td>
+            <td style={{ textAlign: 'center' }}>
+              <div className="stats shadow">
+
+                <div className="stat">
+                  <div className="stat-figure text-primary">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
+                  </div>
+                  <div className="stat-title">Polubień</div>
+                  <div className="stat-value text-primary">25.6K</div>
+                </div>
+
+                <div className="stat">
+                  <div className="stat-figure text-secondary">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
+                  </div>
+                  <div className="stat-title">Ukończyło</div>
+                  <div className="stat-value text-secondary">29k</div>
+                </div>
+
+              </div>
+            </td>
+            <th>
+              <button className="btn btn-primary">Oceń i oznacz</button>
+            </th>
+          </tr>
+          {/* row 5 */}
+          <tr>
+            <td style={{ textAlign: 'center' }}>
+              <div className="indicator">
+                <span className="indicator-item badge badge-secondary" style={{fontWeight: 'bold'}}>modified</span>
+                <div className="grid w-8 h-8 bg-base-100 place-items-center" style={{fontSize: '30px'}}>5</div>
+              </div>
+            </td>
+            <td style={{ textAlign: 'center' }}>
+              <div className="flex items-center justify-center">
+                <div className="avatar">
+                  <div className="mask w-40 h-40 relative">
+                    <span className="indicator-item indicator-top indicator-start badge badge-primary" style={{ fontSize: '20px', fontWeight: 'bold' }}>Begginer turn</span>
+                    <img src="src/assets/img/CS.jpg" alt="Climbing Spot" />
+                  </div>
+                </div>
+              </div>
+            </td>
+            <td style={{ textAlign: 'center', fontSize: '30px' }}>Tosia</td>
+            <td style={{ textAlign: 'center', fontSize: '30px' }}>Biały</td>
+            <td style={{ textAlign: 'center' }}>
+              <div className="stats shadow">
+
+                <div className="stat">
+                  <div className="stat-figure text-primary">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
+                  </div>
+                  <div className="stat-title">Polubień</div>
+                  <div className="stat-value text-primary">25.6K</div>
+                </div>
+
+                <div className="stat">
+                  <div className="stat-figure text-secondary">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
+                  </div>
+                  <div className="stat-title">Ukończyło</div>
+                  <div className="stat-value text-secondary">29k</div>
+                </div>
+
+              </div>
+            </td>
+            <th>
+              <button className="btn btn-primary">Oceń i oznacz</button>
+            </th>
+          </tr>
+          {/* row 6 */}
+          <tr>
+            <td style={{ textAlign: 'center' }}>
+              <div className="indicator">
+                <div className="grid w-8 h-8 bg-base-100 place-items-center" style={{fontSize: '30px'}}>6</div>
+              </div>
+            </td>
+            <td style={{ textAlign: 'center' }}>
+              <div className="flex items-center justify-center">
+                <div className="avatar">
+                  <div className="mask w-40 h-40 relative">
+                    <span className="indicator-item indicator-top indicator-start badge badge-primary" style={{ fontSize: '20px', fontWeight: 'bold' }}>Begginer turn</span>
+                    <img src="src/assets/img/CS.jpg" alt="Climbing Spot" />
+                  </div>
+                </div>
+              </div>
+            </td>
+            <td style={{ textAlign: 'center', fontSize: '30px' }}>Tosia</td>
+            <td style={{ textAlign: 'center', fontSize: '30px' }}>Biały</td>
+            <td style={{ textAlign: 'center' }}>
+              <div className="stats shadow">
+
+                <div className="stat">
+                  <div className="stat-figure text-primary">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"></path></svg>
+                  </div>
+                  <div className="stat-title">Polubień</div>
+                  <div className="stat-value text-primary">25.6K</div>
+                </div>
+
+                <div className="stat">
+                  <div className="stat-figure text-secondary">
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="inline-block w-8 h-8 stroke-current"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 10V3L4 14h7v7l9-11h-7z"></path></svg>
+                  </div>
+                  <div className="stat-title">Ukończyło</div>
+                  <div className="stat-value text-secondary">29k</div>
+                </div>
+
+              </div>
+            </td>
+            <th>
+              <button className="btn btn-primary">Oceń i oznacz</button>
+            </th>
+          </tr>
+          </tbody>
+          {/* foot */}
+          <tfoot>
+          <tr>
+            <th style={{ textAlign: 'center', fontSize: '15px', fontWeight: 'bold' }}>Nazwa</th>
+            <th style={{ textAlign: 'center', fontSize: '15px', fontWeight: 'bold' }}>Miasto</th>
+            <th style={{ textAlign: 'center', fontSize: '15px', fontWeight: 'bold' }}>Adres</th>
+            <th style={{ textAlign: 'center', fontSize: '15px', fontWeight: 'bold' }}>Ilość tras</th>
+            <th></th>
+          </tr>
+          </tfoot>
+
+        </table>
+      </div>
+
     </>
   );
 }
