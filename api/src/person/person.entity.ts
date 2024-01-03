8f15090eee03e3347107ed9aa19a96ac86065c08
Konrad Szychowiak <konrad.szychowiak@gmail.com>
2024-01-03 17:52:26 +0100

    feat(api): Add passwd reset and two-step registration

--- api/src/person/person.entity.ts
+++ api/src/person/person.entity.ts
@@ -4,9 +4,15 @@ import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 export class Person {
   @PrimaryGeneratedColumn() id!: string;
 
-  @Column() username!: string;
+  @Column({ default: false }) isAdmin!: boolean;
+
+  @Column({ default: false }) isActive!: boolean;
+
+  @Column({ unique: true }) username!: string;
 
   @Column() password!: string;
+
+  @Column({ unique: true }) email!: string;
 
   @Column({ name: 'created_at', default: new Date(Date.now()) })
   createdAt?: Date;
--- api/src/person/person.service.ts
+++ api/src/person/person.service.ts
@@ -1,5 +1,5 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { Repository } from 'typeorm';
+import { ObjectId, Repository } from 'typeorm';
 import { Person } from './person.entity';
 import { PERSON_REPO } from './person.provider';
 
@@ -9,6 +9,18 @@ export class PersonService {
     @Inject(PERSON_REPO)
     private personRepository: Repository<Person>,
   ) {}
+
+  async activate(id: number | string | ObjectId) {
+    return this.personRepository.update(id, { isActive: true });
+  }
+
+  async resetPassword(id: number | string | ObjectId, password: string) {
+    return this.personRepository.update(id, { isActive: true, password });
+  }
+
+  async deactivate(id: number | string | ObjectId) {
+    return this.personRepository.update(id, { isActive: false });
+  }
 
   async findAll(): Promise<Person[]> {
     return this.personRepository.find();
@@ -20,5 +32,9 @@ export class PersonService {
 
   async findOneByUsername(username: string) {
     return this.personRepository.findOneBy({ username });
+  }
+
+  async findOneByEmail(email: string) {
+    return this.personRepository.findOneBy({ email });
   }
 }
