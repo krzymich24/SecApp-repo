147bdf168fe098dd39d1a4f0c85ca0b3438ef935
krzymich24 <krzysztof.michalak1999@gmail.com>
2024-01-15 21:09:06 +0100

    adding form for creating new gyms

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -42,7 +42,7 @@ function Header() {
               </Link>
             </li>
             <li>
-              <Link to="/climbingwalls">Climbing Walls
+              <Link to="/gym">Climbing Walls
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
                      stroke="currentColor" className="w-6 h-6">
                   <path strokeLinecap="round" strokeLinejoin="round"
