41205795f0d7100dda68f6b7be93288a075b6bca
krzymich24 <krzysztof.michalak1999@gmail.com>
2024-01-15 21:53:38 +0100

    Update AdmView.tsx

--- react/src/components/AdmView.tsx
+++ react/src/components/AdmView.tsx
@@ -20,15 +20,30 @@ export function AdmView() {
     <div className="flex justify-center">
       <div className={"container"}>
         <header className={"bg-base-200 sticky top-0 z-10"}>
-          <h1 style={{ fontSize: "40px", fontWeight: "bold" }}>News Feed</h1>
+          <h1 style={{ fontSize: "40px", fontWeight: "bold" }}>Admin Panel</h1>
           <p style={{ fontSize: "12px" }}>03.01.2024</p>
           <div className="divider"></div>
         </header>
         <main>
           <div className="columns-3">
-            {gyms.map(({ id,  image, location, name}) =>
-              <GymCard  id={id} image={image} location={location} name={name}/>
-            )}
+            <div>
+              <p>Name of the gym</p>
+              {gyms.map(({ id,  image, location, name}) =>
+                <GymCard  id={id} image={image} location={location} name={name}/>
+              )}
+            </div>
+            <div>
+              <p>List of the route-setters</p>
+              {gyms.map(({ id,  image, location, name}) =>
+                <GymCard  id={id} image={image} location={location} name={name}/>
+              )}
+            </div>
+            <div>
+              <p>List of rest users</p>
+              {gyms.map(({ id,  image, location, name}) =>
+                <GymCard  id={id} image={image} location={location} name={name}/>
+              )}
+            </div>
           </div>
 
         </main>
