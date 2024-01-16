a97f790a8d8235e87d929d93041639341412c919
krzymich24 <krzysztof.michalak1999@gmail.com>
2024-01-16 12:23:18 +0100

    Update main.tsx

--- react/src/main.tsx
+++ react/src/main.tsx
@@ -41,7 +41,7 @@ function Header() {
               </Link>
             </li>
             <li>
-              <Link to="/gym">Climbing Walls
+              <Link to="/gym">Gyms
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5}
                      stroke="currentColor" className="w-6 h-6">
                   <path strokeLinecap="round" strokeLinejoin="round"
