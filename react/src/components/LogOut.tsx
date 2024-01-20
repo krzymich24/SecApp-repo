15b7410063a29a8ebd3e810b0c537ae81378790d
krzymich24 <krzysztof.michalak1999@gmail.com>
2024-01-21 00:09:04 +0100

    Styling LogOut page

--- react/src/components/LogOut.tsx
+++ react/src/components/LogOut.tsx
@@ -6,6 +6,15 @@ import { API } from "../api";
 export function LogOut() {
 
   return (
-    <p>Logged Out</p>
+    <div className="hero min-h-screen bg-base-200">
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="text-center lg:text-left">
+          <h1 className="text-6xl font-bold">Logged Out</h1>
+          <p className="py-6">
+            Log in again or check <a href="#/home" style={{ textDecoration: 'underline' }}>main page</a>
+          </p>
+        </div>
+      </div>
+    </div>
   );
 }
